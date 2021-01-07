b3cbd07a89973be64387b3f74505ab0eee736bd3
itsamelaurio <mslaurabarba@gmail.com>
2021-01-07 17:30:40 +0100

    Snyggade till koden och lade till kommentarer

--- script.js
+++ script.js
@@ -1,7 +1,10 @@
 var $carousel = $('.carousel').flickity({
     prevNextButtons: false
   });
+
 function getTweets(name){
+    // Metod som söker och hämtar tweets om en person.
+    // Args: name: Namnet på personen som tweets ska handla om
     return function(){
         
         $carousel.flickity('destroy');
@@ -13,26 +16,20 @@ function getTweets(name){
             url: "http://localhost:4567/api/v1/tweet/"+tempname,
             headers: {"Accept": "application/json"},
             error: function(response) {
-
                 $carousel = $('.carousel').flickity({
                     wrapAround: true, 
                     prevNextButtons: false,
                     pageDots:false                  
                   });
-
                   $('.carousel').append('<div class="carousel-cell"> <p>Inga tweets hittade</p>');
                   $('#container main').css({"height" : "70vh"}); 
             } 
-            })
+        })
 
-            .done(function(details)
-            {
-                                
-                console.log(details);
-                
+            .done(function(details){    
+                                                                         
                 for(i = 0; i < details.length; i++){
-                    $('.carousel').append('<div class="carousel-cell"> <p>'+details[i]["författare"]+'</p> <p>'+details[i]["text"]+'</p> <p> <a href ='+details[i]['url']+'> Klicka här för att se tweeten på twitters hemsida</a> </p> <img src="loggor/twitter.png">  <p class ="tid">'+details[i]["datum"]+'</p></div>');           
-                    
+                    $('.carousel').append('<div class="carousel-cell"> <p>'+details[i]["författare"]+'</p> <p>'+details[i]["text"]+'</p> <p> <a href ='+details[i]['url']+' target="_blank"> Klicka här för att se tweeten på twitters hemsida</a> </p> <img src="loggor/twitter.png">  <p class ="tid">'+details[i]["datum"]+'</p></div>');           
                 }
                 
                 $carousel = $('.carousel').flickity({
@@ -43,26 +40,28 @@ function getTweets(name){
                   });
                 
             });
-        }
+    }
 }
 
 function getLink(id){
+    // Metod som hämtar en länk till mer information om en person
+    // Args: id: id-numret på personen som valts
     return function(){
         $.ajax({
             url: "http://localhost:4567/api/v1/link/"+id,
             headers: {"Accept": "application/json"} 
-            })
+        })
 
-            .done(function(details)
-            {
-                $('#link').html('<a href= "'+details+'" target="_blank"> Mer info</a>');
-            });
+        .done(function(details){
+            $('#link').html('<a href= "'+details+'" target="_blank"> Mer info</a>');
+        });
     }
 }
 
 
 function updatePersonInfo(id){
-    // metod som uppdaterar informationen på sidan efter vald person
+    // Metod som hämtar information om en vald person och uppdaterar sidan efter det
+    // Args: id: id-numret på personen som valts
     return function(){
         
         $.ajax({
@@ -86,13 +85,15 @@ function updatePersonInfo(id){
 }
 
 $(document).ready(function(){
+    // Metod som ger sidan sitt startutseende. Söker efter partinamn att sortera efter och söker efter alla namn att lägga till i listan
     $('#container main').css({"height" : "70vh"}); 
+
     $.ajax({
         url: "http://localhost:4567/api/v1/partier",
         headers: {"Accept": "application/json"} 
-        })
-    .done(function(partier)
-    {
+    })
+
+    .done(function(partier){
         var i;
         for(i=0;i<partier.length;i++){
            $('#party').append('<option value="'+partier[i]+'">'+partier[i]+'</option>');
@@ -104,7 +105,7 @@ $(document).ready(function(){
 });
 
 function submitForm(){
-    
+    // Metod som körs när en förändring har gjorts i dropdown-forumuläret
     var parti =  $("#party").val();
 
     if (parti == "alla"){
@@ -114,14 +115,13 @@ function submitForm(){
 
     $('ul').empty();
 
-        $.ajax({
+    $.ajax({
         url: "http://localhost:4567/api/v1/ledamoter/parti/" +parti,       
         headers: {"Accept": "application/json"} 
-        })
+    })
     
-        .done(function(data)
-        {
-            var i;
+    .done(function(data){
+        var i;
         for(i = 0; i < data.length; i++){
             html = ' <li id="person_' + i + '">' + data[i]['namn'] + '</li>';
         
@@ -140,36 +140,37 @@ function submitForm(){
     $.ajax({
         url: "http://localhost:4567/api/v1/ledamoter",
         headers: {"Accept": "application/json"} 
-        })
+        
+    })
     
-        .done(function(data)
-        {
-            var i;
-            for(i = 0; i < data.length; i++){
+    .done(function(data){
+        var i;
+        for(i = 0; i < data.length; i++){
             html = ' <li id="person_' + i + '">' + data[i]['namn'] + '</li>';
-          
+            
             $('#personer').append(html);
             $('#personer').append('\n');
             $('#person_' + i).click(updatePersonInfo(i));
             $('#person_' + i).click(getTweets(data[i]['namn']));
             $('#person_' + i).click(getLink(data[i]['id']));
     
-            }; 
+        }; 
         });
   }
 
-  function parallax_height() {
+function parallax_height() {
+    // Metod som kontrollerar parallaxen på sida
     var scroll_top = $(this).scrollTop();
     var sample_section_top = $(".sample-section").offset().top;
     var header_height = $(".sample-header-section").outerHeight();
     $(".sample-section").css({ "margin-top": header_height });
     $(".sample-header").css({ height: header_height - scroll_top });
-  }
+}
+  
+parallax_height();
+$(window).scroll(function() {
     parallax_height();
-    $(window).scroll(function() {
-        parallax_height();
-    });
-    $(window).resize(function() {
-        parallax_height();
-    });
-
+});
+$(window).resize(function() {
+    parallax_height();
+});
